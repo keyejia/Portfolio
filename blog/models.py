a5f1936531e78a78d3d281d40a01d66751b6c715
keyejia <keyejia@gmail.com>
2018-06-26 05:15:34 +0000

    Chore: added home page

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class blog(models.Model):
 	title = models.CharField(max_length=200)
 	date = models.DateField()
 	image = models.ImageField(upload_to='images/')
-	body = models.CharField(max_length=5000)
+	body = models.TextField(max_length=5000)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import Projects.views
 
 urlpatterns = [
+	path('', Projects.views.home, name='home'), 
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
