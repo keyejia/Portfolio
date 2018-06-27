98d03888d2e1468364f4057a4ad2e25fcde20714
keyejia <keyejia@gmail.com>
2018-06-27 03:55:47 +0000

    Chore: added blog html and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import blog
 # Create your views here.
+def allblogs(request):
+	blogs = blog.objects
+	return render(request, 'blog/allblogs.html', {'blogs':blogs})
