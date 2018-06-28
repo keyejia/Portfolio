158daf02ae5f9930a680f3f9f7ef94fcd47436ea
keyejia <keyejia@gmail.com>
2018-06-28 06:18:18 +0000

    Chore: final polish

--- Projects/models.py
+++ Projects/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class Project(models.Model):
+	title = models.CharField(max_length=200)
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length=200)
+	langAndFram = models.CharField(max_length=500)
+	appURL = models.URLField(max_length=200)
+	SourceURL = models.URLField(max_length=400)
+
+	def __str__(self):
+		return self.title
