4bee85902e6717fc64bff63d191d825b70e68b10
keyejia <keyejia@gmail.com>
2018-06-28 03:32:33 +0000

    Chore added static files and directory

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,10 @@ class blog(models.Model):
 	title = models.CharField(max_length=200)
 	date = models.DateField()
 	image = models.ImageField(upload_to='images/')
-	body = models.TextField(max_length=5000)
+	body = models.TextField(max_length=10000)
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:250]
