4b69a0dde5b72a11eb1e85c04e35fe33899895c5
keyejia <keyejia@gmail.com>
2018-07-17 04:00:09 +0000

    Chore: added local setting import

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# try:
-#     from .local_settings import *
-# except ImortError:
-#     pass
+ try:
+     from .local_settings import *
+ except ImortError:
+     pass
